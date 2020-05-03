4d043add2687ff8b5f391a52dc25cbc50cf3966c
Rizwan <rizwansm@outlook.com>
2020-05-03 19:49:12 +0600

    scrapped from iedcr

--- covidtracker/home/views.py
+++ covidtracker/home/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render,get_object_or_404, redirect
 from .models import Home
 import requests
+from bs4 import BeautifulSoup
+import re
 # Create your views here.
 
 
 def home(request):
 	sitename = 'COVID-19 Tracker'
-	link = requests.get('https://disease.sh/v2/countries/BGD?yesterday=true&strict=false')
+	link = requests.get('https://disease.sh/v2/countries/BD')
 	link = link.json()
 
-	last_updated = link['updated']
 	by_division = Home.objects.latest('date')
-	#print(link)
-	args = {'sitename':sitename,'data':link,'by_division':by_division}
+
+	url = 'https://iedcr.gov.bd/'
+	response = requests.get(url)
+
+	info= re.findall(r'<h3>(\d+)</h3>',response.text)
+
+	x = info[0]
+	infos= re.findall(r'<td>(\d+)</td>',response.text)
+	y = infos[8]
+
+
+
+	args = {'sitename':sitename,'data':link,'by_division':by_division,'x':x,'y':y}
 	return render(request,'front/home.html',args)
