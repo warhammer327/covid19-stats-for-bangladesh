b44049ec4fb843dae3a045f1045b86deff3ef8cd
Rizwan <rizwansm@outlook.com>
2020-05-03 09:26:28 +0600

    front page done

--- covidtracker/home/views.py
+++ covidtracker/home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render,get_object_or_404, redirect
 from .models import Home
+import requests
 # Create your views here.
 
 
 def home(request):
 	sitename = 'COVID-19 Tracker'
-	return render(request,'front/home.html',{'sitename':sitename})
+	link = requests.get('https://disease.sh/v2/countries/BGD?yesterday=true&strict=false')
+	link = link.json()
+
+	last_updated = link['updated']
+
+	#print(link)
+	args = {'sitename':sitename,'data':link}
+	return render(request,'front/home.html',args)
